6f6c4bab8dddd6c06daa030dfda68b5b7106c282
Emmanuel <emmanuel.herrmann@etu.univ-orleans.fr>
2020-02-14 16:29:31 +0100

    Set up SQL component

--- backend/src/main/java/fr/octopus/technight/config/FakeNewsConfig.java
+++ backend/src/main/java/fr/octopus/technight/config/FakeNewsConfig.java
@@ -3,19 +3,34 @@ package fr.octopus.technight.config;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class FakeNewsConfig extends RouteBuilder {
 
+    private final DataSource dataSource;
+
+    public FakeNewsConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void configure() {
+
         from("rest:get:generation")
             .bean(NumberGenerator.class)
                 .choice()
                 .when(simple("${body} == 'singular'"))
-                    .log("yay")
+                    .setHeader("singular", constant("1"))
                 .otherwise()
-                    .log("nay")
-                .end();
+                    .setHeader("singular", constant("0"))
+                .end()
+            .to("direct:database");
+
+        from("direct:database")
+            .to("sql:select action_name from action where singular=:#${headers.singular} order by rand() limit 1?outputType=SelectOne")
+            .log("${body}")
+            .log("${headers}");
     }
 
     static class NumberGenerator {
---
+++ backend/src/main/resources/actions.sql
@@ -0,0 +1,3 @@
+INSERT INTO action(action_name) VALUE ('Action 1');
+INSERT INTO action(action_name) VALUE ('Action 2');
+INSERT INTO action(action_name) VALUE ('Action 3');
