842e09559bac03ab582fbc33127893d8cb90ece3
Emmanuel <emmanuel.herrmann@etu.univ-orleans.fr>
2020-02-25 21:03:58 +0100

    Update insert SQL script

--- backend/src/main/resources/actions.sql
+++ backend/src/main/resources/actions.sql
@@ -1,3 +1,28 @@
-INSERT INTO action(action_name) VALUE ('Action 1');
-INSERT INTO action(action_name) VALUE ('Action 2');
-INSERT INTO action(action_name) VALUE ('Action 3');
+INSERT INTO action(action_name, singular)
+VALUES (''aiment crier Allahou Akbar pour emmerder'', 0);
+INSERT INTO action(action_name, singular)
+VALUES (''aime crier Allahou Akbar pour emmerder'', 1);
+INSERT INTO action(action_name, singular)
+VALUES ('volent', 0);
+INSERT INTO action(action_name, singular)
+VALUES ('vole', 1);
+INSERT INTO action(action_name, singular)
+VALUES ('ont tué', 0);
+INSERT INTO action(action_name, singular)
+VALUES ('a tué', 1);
+INSERT INTO action(action_name, singular)
+VALUES ('cachent des informations troublantes sur', 0);
+INSERT INTO action(action_name, singular)
+VALUES ('cache des informations troublantes sur', 1);
+INSERT INTO action(action_name, singular)
+VALUES ('cherchent à détruire', 0);
+INSERT INTO action(action_name, singular)
+VALUES ('cherche à détruire', 1);
+INSERT INTO action(action_name, singular)
+VALUES ('ont répandu le coronavirus pour tuer', 0);
+INSERT INTO action(action_name, singular)
+VALUES ('a répandu le coronavirus pour tuer', 1);
+INSERT INTO action(action_name, singular)
+VALUES ('ont volé les primes d\'Excilys pour plaire à', 0);
+INSERT INTO action(action_name, singular)
+VALUES ('a volé les primes d\'Excilys pour plaire à', 1);
